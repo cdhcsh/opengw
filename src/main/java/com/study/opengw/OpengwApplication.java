bde49b9c4e60e4b23a69fec31a21ab4a71a4256d
cdhcsh <75022174+cdhcsh@users.noreply.github.com>
2022-04-24 14:20:06 +0900

    최초등록
    - 프로젝트 등록

---
+++ src/main/java/com/study/opengw/OpengwApplication.java
@@ -0,0 +1,14 @@
+package com.study.opengw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpengwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpengwApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/study/opengw/OpengwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.opengw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpengwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
